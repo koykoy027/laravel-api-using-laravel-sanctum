7c8747c77f9842d913559d718dbfb326d36263e4
koykoy027 <villanuevajoshua27@gmail.com>
2024-05-25 23:15:32 +0800

    allowed origins

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => explode(',', env('ALLOWED_ORIGINS', '*')),
 
     'allowed_headers' => ['*'],
 
